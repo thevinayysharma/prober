40fce8ee088804b0be274594a79d2a36b4c2a0ac
Vinay Sharma <vinay02856@gmail.com>
2022-06-10 02:05:53 +0530

    Initil Commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Prober
+Simplified JS testing framework for learning.
+
+![Test runner ouput](https://i.ibb.co/GTmhcRj/test-file.png)
+
+## Run Locally
+- Clone this repo.
+- Type ```yarn install``` to add devDependencies locally on your machines.
+- Run tests with ```node index.mjs```. 
+
+## Self_notes
+- .mjs used for Ecmascript files
+- using prettier from, format document in VsCode.
+- echo "message" > filename  ] for writing directly.
+- ```descrive & it ``` are grouping functions in testing framework.
+ rest notes got deleted due to git error :/
+
+## Building Steps
+1. Build a test file system analyzer using haste-map.
+2. Run test in parallel using web workers
+3. Use assertion framework for writing tests & for reporting results.
+4. Isolate test files states. Tada
+
+## Further Learning
+- [Jest repo : gitub](https://github.com/facebook/jest)
+- [Haste-map learning](https://github.com/facebook/jest/blob/04b75978178ccb31bccb9f9b2f8a0db2fecc271e/packages/jest-haste-map/src/index.ts#L136)
+
+## Resources
+- Testing framework Inspired from [https://cpojer.net/posts/building-a-javascript-testing-framework] blog series
---
+++ index.mjs
@@ -0,0 +1,63 @@
+// https://cpojer.net/posts/building-a-javascript-testing-framework
+
+import JestHasteMap from 'jest-haste-map';
+import {cpus} from 'os';
+import {dirname} from 'path';
+import {fileURLToPath} from 'url';
+import fs from 'fs';
+import {Worker} from 'jest-worker';
+import {join, relative} from 'path';
+import chalk from 'chalk';
+
+const root = dirname(fileURLToPath(import.meta.url));
+const hasteMapOptions = {
+  extensions: ['js'],
+  maxWorkers: cpus().length,
+  name: 'best-test-framework',
+  platforms: [],
+  rootDir: root,
+  roots: [root],
+};
+const hasteMap = new JestHasteMap.default(hasteMapOptions);
+await hasteMap.setupCachePath(hasteMapOptions);
+const {hasteFS} = await hasteMap.build();
+const testFiles = hasteFS.matchFilesWithGlob([
+  process.argv[2] ? `**/${process.argv[2]}*` : '**/*.test.js',
+]);
+const worker = new Worker(join(root, 'worker.js'), {
+  enableWorkerThreads: true,
+});
+let hasFailed = false;
+
+await Promise.all(
+  Array.from(testFiles).map(async (testFile) => {
+    const {success, testResults, errorMessage} = await worker.runTest(testFile);
+    const status = success
+      ? chalk.green.inverse.bold(' PASS ')
+      : chalk.red.inverse.bold(' FAIL ');
+
+    console.log(status + ' ' + chalk.dim(relative(root, testFile)));
+    if (!success) {
+      hasFailed = true;
+      if (testResults) {
+        testResults
+          .filter((result) => result.errors.length)
+          .forEach((result) =>
+            console.log(
+              result.testPath.slice(1).join(' ') + '\n' + result.errors[0],
+            ),
+          );
+      } else if (errorMessage) {
+        console.log('  ' + errorMessage);
+      }
+    }
+  }),
+);
+
+worker.end();
+if (hasFailed) {
+  console.log(
+    '\n' + chalk.red.bold('Test run failed, please fix all the failing tests.'),
+  );
+  process.exitCode = 1;
+}
